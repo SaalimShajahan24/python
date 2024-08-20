a63f02d9063aa475e8e270b5693ca976c5842678
SaalimShajahan24 <162309843+SaalimShajahan24@users.noreply.github.com>
2024-08-20 11:41:12 +0530

    Add files via upload

---
+++ 1.9.py
@@ -0,0 +1,9 @@
+
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+
+print("Union of A and B:", A | B)
+print("Intersection of A and B:", A & B)
+print("Difference of A and B:", A - B)
+print("Difference of B and A:", B - A)
