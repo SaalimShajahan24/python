e8360ac2497869f7299c180df0e29b0a0e92526a
SaalimShajahan24 <162309843+SaalimShajahan24@users.noreply.github.com>
2024-09-07 21:01:43 +0530

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,10 @@
+class sem3:
+   def __init__(self,name):
+     self.name=name
+   def duties(self):
+      print('Attendance')
+clrep=sem3("Siavnand")
+plrep=sem3("Merin")
+print(clrep.name)
+print(plrep.name)
+clrep.duties()
