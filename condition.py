a1ed8f109c59b4ad926e0b12ab9b4fbe4a3b1edd
SaalimShajahan24 <162309843+SaalimShajahan24@users.noreply.github.com>
2024-09-08 10:13:57 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,12 @@
+l1=['sam','Abey','sanjay','revathi','sarath']
+'''if 'revathi' in list1:
+    print("name is present")
+else:
+    print("name is not present")
+l1.sort()
+for n in l1:
+    print(n)'''
+n=0
+while n<len(l1):
+    print(l1[n])
+    n=n+1
